aaf97ff5ee864c07f5249617f5d3f304721e7d8f
ksm7893 <ksm7893@naver.com>
2024-12-10 10:46:32 +0900

    version/down

---
+++ naverProject/bin/Source/PageView/naverProject000Copy.js
@@ -0,0 +1,38 @@
+
+/**
+Constructor
+Do not call Function in Constructor.
+*/
+function naverProject000Copy()
+{
+	AView.call(this);
+
+	//TODO:edit here
+
+}
+afc.extendsClass(naverProject000Copy, AView);
+
+
+naverProject000Copy.prototype.init = function(context, evtListener)
+{
+	AView.prototype.init.call(this, context, evtListener);
+
+	//TODO:edit here
+
+};
+
+naverProject000Copy.prototype.onInitDone = function()
+{
+	AView.prototype.onInitDone.call(this);
+
+	//TODO:edit here
+
+};
+
+naverProject000Copy.prototype.onActiveDone = function(isFirst)
+{
+	AView.prototype.onActiveDone.call(this, isFirst);
+
+	//TODO:edit here
+
+};
